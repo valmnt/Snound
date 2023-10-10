8f453a00edf0ade331dd731403e4b8c732706a11
valmnt <valentinmont8@gmail.com>
2023-10-10 21:07:28 +0200

    [Sniffer] Resizable gradient background

--- Snound/UI/SnifferViewController.swift
+++ Snound/UI/SnifferViewController.swift
@@ -8,21 +8,29 @@
 import UIKit
 
 class SnifferViewController: UIViewController {
+    
+    lazy var backgroundColor: CAGradientLayer = {
+        let gradient = CAGradientLayer()
+        gradient.colors = [
+            UIColor(resource: R.color.background)!.withAlphaComponent(0.4).cgColor,
+            UIColor(resource: R.color.background)!.withAlphaComponent(0.5).cgColor,
+            UIColor(resource: R.color.background)!.withAlphaComponent(0.6).cgColor,
+        ]
+        return gradient
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupBackgroundColor()
+        view.backgroundColor = .gray
+        backgroundColor.frame = view.bounds
+        view.layer.insertSublayer(backgroundColor, at: 0)
     }
     
-    func setupBackgroundColor() {
-        let gradient = CAGradientLayer()
-        gradient.frame = view.bounds
-        gradient.colors = [
-            UIColor(resource: R.color.background)!.withAlphaComponent(0.6).cgColor,
-            UIColor(resource: R.color.background)!.withAlphaComponent(0.7).cgColor,
-            UIColor(resource: R.color.background)!.withAlphaComponent(0.8).cgColor,
-        ]
-        view.layer.insertSublayer(gradient, at: 0)
+    override func viewWillTransition(to size: CGSize,
+                                     with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        backgroundColor.frame.size = size
+        view.layer.insertSublayer(backgroundColor, at: 0)
     }
 }
 
