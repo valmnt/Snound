ec3d232bd943d2492474ab4f119179cd298d187a
valmnt <valentinmont8@gmail.com>
2023-11-09 18:15:01 +0100

    [SHMusicList] Add title

--- Snound/UI/SHMusicList/SHMusicListViewController.swift
+++ Snound/UI/SHMusicList/SHMusicListViewController.swift
@@ -26,23 +26,37 @@ class SHMusicListViewController: SNViewController {
         return view
     }()
     
+    private lazy var yourMusicLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Your Music"
+        label.tintColor = .white
+        label.font = .boldSystemFont(ofSize: 27)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override var backgroundColor: UIColor? {
         UIColor(resource: R.color.backgroundLight)
     }
     
     override func viewDidLoad() {
         view.addSubview(topBar)
-        setupConstraints()
+        view.addSubview(yourMusicLabel)
+        setupSmallConstraints()
         super.viewDidLoad()
     }
     
-    private func setupConstraints() {
-        sharedConstraints.append(contentsOf: [
+    private func setupSmallConstraints() {
+        sharedConstraints = [
             topBar.topAnchor.constraint(equalTo: view.topAnchor, constant: 15),
             topBar.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             topBar.widthAnchor.constraint(equalToConstant: 100),
             topBar.heightAnchor.constraint(equalToConstant: 10),
-        ])
+            
+            yourMusicLabel.topAnchor.constraint(equalTo: topBar.bottomAnchor, constant: 40),
+            yourMusicLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            yourMusicLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ]
     }
 }
 
