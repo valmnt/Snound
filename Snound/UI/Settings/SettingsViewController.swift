093632bd4580fa790e7c8928c560eac01865a926
valmnt <valentinmont8@gmail.com>
2023-11-21 20:29:39 +0100

    [Settings] Implement warning text

--- Snound/UI/Settings/SettingsViewController.swift
+++ Snound/UI/Settings/SettingsViewController.swift
@@ -6,11 +6,54 @@
 //
 
 import Foundation
+import UIKit
 
 class SettingsViewController: SNViewController {
     
+    lazy var warningLabel: UILabel = {
+        let label = UILabel()
+        label.text = R.string.settings.deletionWarning.callAsFunction()
+        label.textColor = .white
+        label.numberOfLines = 5
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
+        compactCallback = { [weak self] in
+            guard let self = self else { return }
+            self.warningLabel.font = .systemFont(ofSize: 18)
+        }
+        
+        regularCallback = { [weak self] in
+            guard let self = self else { return }
+            self.warningLabel.font = .systemFont(ofSize: 28)
+        }
+        
+        view.addSubview(warningLabel)
+        
+        setupConstraints()
         super.viewDidLoad()
         navigationController?.navigationBar.tintColor = .white
+    }
+    
+    private func setupConstraints() {
+        sharedConstraints.append(contentsOf: [
+            warningLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            
+        ])
+        
+        compactConstraints.append(contentsOf: [
+            warningLabel.heightAnchor.constraint(equalToConstant: 100),
+            warningLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            warningLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+        ])
+        
+        regularConstraints.append(contentsOf: [
+            warningLabel.heightAnchor.constraint(equalToConstant: 200),
+            warningLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            warningLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+        ])
     }
 }
